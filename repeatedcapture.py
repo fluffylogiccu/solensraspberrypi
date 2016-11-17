928f1ef941d549a98a7da854679e0b65f4969592
jrapp <jrapp@github.com>
2016-11-17 21:10:07 +0000

    Will take pictures every thirty seconds in UTC

--- repeatedcapture.py
+++ repeatedcapture.py
@@ -1,1 +1,34 @@
+from datetime import datetime, timedelta
+import picamera
+import sched, time
 
+s = sched.scheduler(time.time, time.sleep)
+
+camera = picamera.PiCamera()
+
+
+def camera_start():
+	#calculate when to schedule the first image capture
+	t = datetime.utcnow()
+	t += timedelta(minutes = 1, seconds = -t.second, microseconds = -t.microsecond)
+	print("First image will be captured at ", t)
+	s.enterabs(time.mktime(t.timetuple()), 1, capture_image)
+	#Start the scheduler
+	s.run()
+
+
+
+def capture_image():
+	#take a picture
+	camera.capture('/home/pi/FluffyLogic/Pictures/testimg1.jpg')
+	#schedule the next image capture
+	t = datetime.utcnow()
+	if t.second == 30:
+		t += timedelta(minutes = 1, seconds = -t.second, microseconds = -t.microsecond)
+	else:
+		t += timedelta(seconds = -t.second + 30, microseconds = -t.microsecond)
+	print(t)
+	s.enterabs(time.mktime(t.timetuple()), 1, capture_image)
+	
+if __name__ == "__main__":
+	camera_start()
