9b2f7e533c87eb75ff6891235b81e0c791f77cd4
jrapp <jrapp@github.com>
2016-11-22 20:16:59 +0000

    Added Logging

--- SolensImageCapture.py
+++ SolensImageCapture.py
@@ -8,9 +8,11 @@ import configparser
 import urllib.request
 import json
 import os
+import logging
 
 class SolensImageCapture:
 	def __init__(self):
+		logging.basicConfig(filename="SolensImageCapture.log",level=logging.DEBUG)
 		self.scheduler = sched.scheduler(time.time, time.sleep)
 		self.config = configparser.ConfigParser()
 		self.camera = picamera.PiCamera()
@@ -23,6 +25,9 @@ class SolensImageCapture:
 		self.lat = self.config['location']['lat']
 		self.lon = self.config['location']['long']
 		self.timezone = self.config['location']['timezone']
+		logging.debug("LAT: " + self.lat)
+		logging.debug("LONG: " + self.lon)
+		logging.debug("TIMEZONE: " + self.timezone)
 		self.sunrise = self.get_sunrise()
 		self.sunset = self.get_sunset()
 		self.s = sched.scheduler(time.time, time.sleep)
@@ -37,8 +42,8 @@ class SolensImageCapture:
 		with urllib.request.urlopen(request_string) as f:
 			data = json.loads(f.read().decode('utf-8'))
 		sunrise = datetime.strptime(data['results']['sunrise'][:-6],'%Y-%m-%dT%H:%M:%S')
-		print("Sunrise")
-		print(sunrise)
+		logging.debug("Sunrise: ")
+		logging.debug(sunrise.isoformat())
 		return sunrise
 		
 	def get_sunset(self, tomorrow=False):
@@ -49,8 +54,8 @@ class SolensImageCapture:
 		with urllib.request.urlopen(request_string) as f:
 			data = json.loads(f.read().decode('utf-8'))
 		sunset = datetime.strptime(data['results']['sunset'][:-6],'%Y-%m-%dT%H:%M:%S')
-		print("Sunset")
-		print(sunset)
+		logging.debug("Sunset: ")
+		logging.debug(sunset.isoformat())
 		return sunset
 		
 	def get_rounded_next_time(self):
@@ -79,16 +84,21 @@ class SolensImageCapture:
 				#Night time, schedule next event for sunrise
 				self.night_time()
 			else:
+				logging.debug("Next scheduled picture: " + t.isoformat())
 				self.s.enterabs(time.mktime(t.timetuple()), 1, self.day_time_loop, argument = (t,))
 				
 	def night_time(self):
+		logging.debug("Going to sleep")
 		#update sunrise and sunset information for the next day
 		self.sunrise = self.get_sunrise(tomorrow=True)
 		self.sunset = self.get_sunset(tomorrow=True)
+		logging.debug("Waking up tomorrow at " + self.sunrise.isoformat())
 		self.s.enterabs(time.mktime(self.sunrise.timetuple()), 1, self.wake_up)
 		
 	def wake_up(self):
+		logging.debug("Initialize image capture sequence...")
 		t = self.get_rounded_next_time()
+		logging.debug("Scheduling first image at " + t.isoformat())
 		self.s.enterabs(time.mktime(t.timetuple()),1,self.day_time_loop, argument = (t,))
 
 
@@ -98,11 +108,11 @@ if __name__ == "__main__":
 	now_time = datetime.utcnow()
 	#now_time += timedelta(hours = 6)
 	if now_time > solens.sunrise and now_time < solens.sunset:
-		print("The sun is up!")
+		logging.debug("The sun is up. Starting image capture")
 		t = solens.get_rounded_next_time()
 		solens.wake_up()
 	else:
-		print("The sun is down!")
+		logging.debug("The sun is down. Configuring wakeup for tomorrow at sunrise")
 		solens.night_time()
 	#start the scheduler
 	solens.s.run()
