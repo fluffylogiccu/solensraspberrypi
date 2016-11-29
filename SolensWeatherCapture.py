b28cdff1bdb4916b789007cbb0eadccd69d852ae
jrapp <jrapp@github.com>
2016-11-29 18:44:10 +0000

    Don't need the RC stuff for the single capture. Started working on python script for temperature and humidity gathering

--- SolensWeatherCapture.py
+++ SolensWeatherCapture.py
@@ -1,1 +1,19 @@
+import sys
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+import Adafruit_DHT
 
+
+class SolensWeatherCapture:
+	def __init__(self):
+		#DO TO MAKE THIS A CONFIG SETTING
+		self.DHTpin = 23
+		
+		
+	def get_temp_and_humidity:
+		RHW, TW = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
+		
+		return (str(RHW), str(TW))
+		
+	
--- singletemperaturecapture.py
+++ singletemperaturecapture.py
@@ -6,12 +6,7 @@ import Adafruit_DHT
 
 
 
-RCpin = 24
 DHTpin = 23
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RCpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 RHW, TW = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
